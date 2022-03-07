18270b0fed508d07798fbe23f273d760045697dc
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-03-07 12:22:21 -0300

    :tada: first commit

---
+++ src/main/java/com/lucasladeira/MicroservicesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lucasladeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesSpringApplication.class, args);
+	}
+
+}
