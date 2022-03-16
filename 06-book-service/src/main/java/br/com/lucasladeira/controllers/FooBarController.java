e85a891342c56919f306a9603ccbb2034f6a6eca
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-03-16 15:22:21 -0300

    Fallback methods com Resilience4j Wait duration

--- 06-book-service/src/main/java/br/com/lucasladeira/controllers/FooBarController.java
+++ 06-book-service/src/main/java/br/com/lucasladeira/controllers/FooBarController.java
@@ -16,11 +16,15 @@ public class FooBarController {
 	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 	
 	@GetMapping("/foo-bar")
-	@Retry(name = "default")
+	@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
 	public String fooBar() {
 		logger.info("Request to foo-bar is received!");
 		new RestTemplate()
 		.getForEntity("http://localhost:8080/foo-bar", String.class);
 		return "Foo-bar!";
+	}
+	
+	public String fallbackMethod(Exception ex) {
+		return "fallbackMethod foo-bar!!" + ex.getMessage();
 	}
 }
